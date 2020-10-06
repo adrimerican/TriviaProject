d9d381cf46917039bf8449a4e45e25e47b73f995
Hamza <hamzam1478@gmail.com>
2020-10-06 11:33:08 +0100

    0/updatedCode

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [ sport, setSport ] = useState({
-    team1: '',
-    team2: '',
-    score: ''
-  })
+const [user, setUser] = useState({
+  email: '',
+  password: '',
+  posts: []
+});
 
-  const [ joke, setJoke ] = useState("");
-  const [ user, setUser ] = useState({
-    email: '',
-    password: ''
-  })
-  useEffect( () => {
-    getApi();
-  }, []);
+useEffect(() => {
+  getApi();
+}, [])
 
-  const getApi = () => {
-    //const res = await axios.get("http://localhost:5000/myApi");
-    const res = await axios.get("https://api.chucknorris.io/jokes/random");
-    console.log(res.data);
-  }
-  // setSport {
-  //   team1: res.data.Team1;
-  //   team2: res.data.Team2;
-  //   score: res.data.Score;
-  // }
+const getApi = async () => {
+    // const res = await axios.get('/myApi')
+    const res = await axios.get('https://opentdb.com/api.php?amount=10')
+    console.log(res.data)
 
-  const updateUser = (event) => {
-console.log(event.target.value);
-
-setUser({
-  ...user,
-  //email
-  [event.target.name]: event.target.value
-})
-
-const sendData = (event) => {
-  event.preventDefault();
+    // setJoke(res.data.value)
+    // setSport({
+    //   team1: res.data.Team1,
+    //   team2: res.data.Team2,
+    //   score: res.data.Score
+    // })
 }
+
+const updateUser = (event) => {
+  console.log(event.target.value);
+
+  setUser({
+    ...user,
+    [event.target.name]: event.target.value
+  })
+}
+
+
+const sendData = async (event) => {
+  event.preventDefault();
+  console.log('Form Submitted'); 
+
+  const body = JSON.stringify({
+    userEmail: user.email,
+    userPassword: user.password
+  });
+
+  const config = {
+    headers: {
+      'content-type' : 'application/json'
+    }
   }
+
+  const res = await axios.post('/register', body, config);
+  // axios.delete('/deleteUser')
+  // axios.put('...')
+  console.log(res.data)
+
+}
+
+  console.log('My state is:');
+  console.log(user);
   return (
-    <div className="App">
-        {/* <h1>The Joke is: {joke}</h1>
-        <h1>Team1:{sport.team1}</h1>
-        <h1>Score:{sport.score}</h1>
-        <h1>Team2:{sport.team2}</h1> */}
+    <div className='App'>
+      {/* <h1>The joke is: {joke}</h1>
+     <h1>Score: {sport.score}</h1>
+      <h1>Team 2: {sport.team2}</h1> */}
 
-        <form onSubmit={sendData}>
-          <label>Email</label>
-          <input type="email" />
-          <label>Password</label>
-          <input type="password" />
-          <button type="submit">Submit</button>
 
-        </form>
+      <form onSubmit={sendData}>
+        <label>Email</label>
+       <input type='email' onChange={updateUser} name='email'/>
+       <label>Password</label>
+       <input type='password' onChange={updateUser} name='password'/>
+       <button type='submit'>Register</button>
+      </form>
     </div>
-  );
+  );  
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,26 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-//const nodemon = require('nodemon');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config( {  path: './.env'  } );
+dotenv.config({ path: "./.env" });
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology:true
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Is now connected."));
 
 app.get("/", (req, res) => {
-    res.send("Hello from main file");
-});
-//.then(() => console.log("MongoDB is connected"));
-
-
-app.listen( 5000, () => {
-    console.log("Server is running on Port 5000");
+  res.send("Hello from NodeJS");
 });
 
+app.get("/myApi", (req, res) => {
+  res.status(200).json({
+    "Team1": "Man UTD",
+    "Score": "1-6",
+    "Team2": "Spurs",
+    "Score": "2-4",
+  });
+});
+
+app.post('/register', (req, res) => {
+    console.log(req.body)
+
+    res.status(200).json({
+      message: 'User Registered'
+    })
+})
+
+app.delete('/deleteUser', (req, res) => {
+  
+})
+
+app.listen(5000, () => {
+  console.log(`Server Is now running at port: 5000`);
+});
