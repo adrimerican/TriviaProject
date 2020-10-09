d7d1719b8e2bd8afa8220169e349a3f97f7b66ce
Adri Roos <adri.roos>
2020-10-09 12:15:18 +0100

    quiz files

---
+++ client/src/components/QuickQuiz App/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Quick Quiz</title>
+    <link rel="stylesheet" href="app.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div id="home" class="flex-center flex-column">
+        <h1>Quick Quiz</h1>
+        <a class="btn" href="choice.html">Play</a>
+        <a class="btn" href="highscores.html">High Scores</a>
+      </div>
+    </div>
+  </body>
+
+</div>
+  
+);
+
+}
+
+export default App;
+
+  
